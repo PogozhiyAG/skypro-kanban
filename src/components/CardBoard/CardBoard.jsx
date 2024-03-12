8f2d86362b14cd8ce107d504002de450e54f9946
aleksandr.pogozhii <aleksandr.pogozhii@devexpress.com>
2024-03-12 22:25:37 +0000

    dnd

--- src/components/CardBoard/CardBoard.jsx
+++ src/components/CardBoard/CardBoard.jsx
@@ -1,14 +1,19 @@
 /* eslint-disable react/prop-types */
+import { DragDropContext } from 'react-beautiful-dnd';
 import { TASK_STATUSES } from '../../data';
 import { CardStatusList } from '../CardStatusList/CardStatusList';
 
 
 export const CardBoard = ({tasks}) => {
+    const onDragEnd = result => {
+        console.log(result);
+    };
+
     return (
-        <>
+        <DragDropContext onDragEnd={onDragEnd}>
             {Object.keys(TASK_STATUSES).map((s, i) => 
                 <CardStatusList key={i} tasks={tasks} statusCode={s}/>
             )}
-        </>
+        </DragDropContext>
     );
 };
